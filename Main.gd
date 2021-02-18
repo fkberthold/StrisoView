481ac2408988bd61379e025fa8256ac36f366b11
Frank Berthold <fkberthold@gmail.com>
2021-02-18 18:22:45 -0500

    Remaining scales added

--- Main.gd
+++ Main.gd
@@ -4,6 +4,7 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var scale_names = []
 
 func set_chords():
 	$Chords/ChordsList.add_item("Major")
@@ -14,11 +15,15 @@ func set_chords():
 	$Chords/ChordsList.add_item("Suspended 2nd")
 	
 func set_scales():
-	$Scales/ScalesList.add_item("Major")
-	$Scales/ScalesList.add_item("Minor")
-	
+	for scale in $SBoard.scales:
+		scale_names.append(scale)
+		$Scales/ScalesList.add_item(scale)
+
+
+
 func set_modes():
-	pass
+	for mode in $SBoard.modes:
+		$Scales/ModesList.add_item(mode)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,11 +32,16 @@ func _ready():
 	set_modes()
 
 
-
 func _on_Chords_item_selected(index):
 	var chord_names = ["major","augmented","minor", "diminished", "sus4", "sus2"]
 	$SBoard.set_chord(chord_names[index], true)
 
 func _on_ScalesList_item_selected(index):
-	var scale_names = ["Major", "Minor"]
 	$SBoard.set_music_scale(scale_names[index])
+	if scale_names[index] == "Major":
+		$Scales/ModesList.visible = true
+	else:
+		$Scales/ModesList.visible = false
+
+func _on_ModesList_item_selected(index):
+	$SBoard.set_music_mode(index)
--- Scenes/SBoard.gd
+++ Scenes/SBoard.gd
@@ -2,8 +2,17 @@ extends Control
 
 var sel_button = null
 const scales = {"Major":["P1", "M2", "M3", "P4", "P5", "M6", "M7"],
-				"Minor":["P1", "M2", "m3", "P4", "P5", "m6", "m7"]}
+				"Natural Minor": ["P1", "M2", "m3", "P4", "P5", "m6", "m7"],
+				"Harmonic Minor": ["P1", "M2", "m3", "P4", "P5", "m6", "M7"],
+				"Harmonic Major":["P1","M2","M3","P4","P5","m6","M7"],
+				"Double Harmonic":["P1","m2","M3","P4","P5","m6","M7"],
+				"Hungarian Minor":["P1","M2","m3","A4","P5","m6","M7"],
+				"Major Pentatonic":["P1","M2","M3","P5","M6"],
+				"Minor Pentatonic":["P1","m3","P4","P5","m7"],
+				"Six-Note Blues":["P1", "m3","P4","A4","P5","m7"]}
+const modes = ["Ionian", "Dorian", "Phrygian", "Lydian", "Mixolydian", "Aeolian", "Locrian"]
 var sel_scale = "Major"
+var sel_mode = 0
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -28,18 +37,36 @@ func button_selected(button, degree, note_name, row, col):
 		
 	var cur_note_name
 	var base_coords = []
+	var least_coord = [INF, INF]
 	
 	if sel_button:
 		cur_note_name = sel_button.get_node("Note").text
-		
+	
 	for button in self.get_children():
 		button.button_selected(null, null, null, null)
 		button.hide_scale()
+		
+	if not sel_button:
+		return
+	
 	for button in self.get_children():
 		button.button_selected(degree, note_name, row, col)
 		if(cur_note_name == button.get_node("Note").text):
-			button.show_root()
+			if button.row < least_coord[0]:
+				least_coord = [button.row, button.col]
 			base_coords.append([button.row, button.col])
+
+	var p8 = [2,1]
+	var below_least_coord = [least_coord[0] - p8[0], least_coord[1] - p8[1]]
+	base_coords.append(below_least_coord)
+	var base_interval
+	
+	if sel_scale == "Major":
+		base_interval = scales[sel_scale][sel_mode]
+	else:
+		base_interval = scales[sel_scale][0]
+	
+		
 	
 	for button in self.get_children():
 		var b_row = button.row
@@ -47,10 +74,10 @@ func button_selected(button, degree, note_name, row, col):
 		var mod_coords = []
 		for base_coord in base_coords:
 			var mod_coord = [b_row - base_coord[0], b_col - base_coord[1]]
-			if mod_coord == [0,0]:
+			var interval = button.relative_int.get(mod_coord)
+			if interval == base_interval:
 				button.show_root()
 				break
-			var interval = button.relative_int.get(mod_coord)
 			if interval in scales[sel_scale]:
 				button.show_scale()
 				break
@@ -62,14 +89,18 @@ func set_chord(chord, state):
 			button.chord_vis = chord
 		else:
 			button.chord_vis = null
+	if sel_button:
+		button_selected(sel_button, sel_button.full_degree, sel_button.get_node("Note").text, sel_button.row, sel_button.col)
 
 func set_music_scale(scale):
 	sel_scale = scale
+	if sel_button:
+		button_selected(sel_button, sel_button.full_degree, sel_button.get_node("Note").text, sel_button.row, sel_button.col)
 
-func display_scale(scale):
-	for button in self.get_children():
-		pass
-		
+func set_music_mode(mode_index):
+	sel_mode = mode_index
+	if sel_button:
+		button_selected(sel_button, sel_button.full_degree, sel_button.get_node("Note").text, sel_button.row, sel_button.col)
 
 func _on_chkSound_toggled(button_pressed):
 	for button in self.get_children():
